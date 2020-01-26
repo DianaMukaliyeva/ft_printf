b40be9d0804aaafadd25be55837d5de46ad1ca37
DianaMukaliyeva <45383538+DianaMukaliyeva@users.noreply.github.com>
2020-01-26 13:23:44 +0200

    add flag l to double

--- src/parse_flags.c
+++ src/parse_flags.c
@@ -32,7 +32,7 @@ static void		fill_other_flags(t_flag *flags, char symbol)
 		flags->l = flags->l == 1 ? 0 : 1;
 	}
 	else if (symbol == 'L')
-		flags->big_l = (flags->big_l || flags->l) ? 0 : 1;
+		flags->big_l = (flags->big_l || flags->l) ? 0 : 1;//big_l and l in double
 	symbol == 'j' ? flags->j = 1 : 0;
 	symbol == 'z' ? flags->z = 1 : 0;
 }
